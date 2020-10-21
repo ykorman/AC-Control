e158fb7619e70bd2cce5ca1180337eae2927f7f0
Yigal Korman <ykorman@gmail.com>
2020-10-21 07:00:26 +0300

    Multiple enchancments
    Fixed virtual env activation.
    
    Improved DEBUG - now only setup is real, everything else is just
    printed.
    
    Added status mode.
    
    Added option to modify script parameter via environment variables.
    
    Signed-off-by: Yigal Korman <ykorman@gmail.com>

--- night_script.sh
+++ night_script.sh
@@ -3,18 +3,41 @@
 DIR=$(dirname $0)
 B=${DIR}/broadlink-cli
 SLEEP_CMD="sleep 1m"
+DEV_CONFIG=${DIR}/device_config
+
+START_DELAY=${START_DELAY:-50}
+TIMER_ON=${TIMER_ON:-10}
+TIMER_OFF=${TIMER_OFF:-50}
+
+STATUS_FILE=/run/$(basename $0).status
+
+if [[ ${EUID} != 0 ]] ; then
+	STATUS_FILE=${DIR}/.$(basename $0).status
+fi
 
 # debug
 if [[ -n ${DEBUG} ]] ; then
 	SLEEP_CMD="break"
 fi
 
-setup() {
-	${B}/broadlink_discovery | tail -n3 | head -n1 > ${DIR}/device_config
-}
-
 log() {
 	echo $(date +%H:%M) -- $*
+}
+
+setup() {
+	source ${DIR}/env/bin/activate
+	${B}/broadlink_discovery | tail -n3 | head -n1 > ${DEV_CONFIG}
+	log "Generated device config:"
+	cat ${DEV_CONFIG}
+	echo
+}
+
+cmd() {
+	if [[ -z ${DEBUG} ]] ; then
+		${B}/broadlink_cli --device @${DIR}/device_config --send $*
+	else
+		log "!DEBUG! Executing cmd with $*"
+	fi
 }
 
 msleep() {
@@ -23,6 +46,7 @@ msleep() {
 
 	log "Sleeping for ${amount} minutes"
 	while [[ ${i} -lt ${amount} ]] ; do
+		echo "$((amount - i)):${2}" > ${STATUS_FILE}
 		echo -n .
 		${SLEEP_CMD}
 		let i++ || :
@@ -30,15 +54,28 @@ msleep() {
 	echo
 }
 
-setup
 
-msleep 50
-while true ; do
-	# turn ac on
-	log "Turning AC on (26 degrees)"
-	${B}/broadlink_cli --device @${DIR}/device_config --send @${DIR}/cmds/ac.26
-	msleep $((9))
-	log "Turning AC off"
-	${B}/broadlink_cli --device @${DIR}/device_config --send @${DIR}/cmds/ac.off
-	msleep 50
-done
+main() {
+	setup
+
+	msleep ${START_DELAY}
+	while true ; do
+		# turn ac on
+		log "Turning AC on (26 degrees)"
+		cmd @${DIR}/cmds/ac.26
+		msleep ${TIMER_ON} "on"
+		log "Turning AC off"
+		cmd @${DIR}/cmds/ac.off
+		msleep ${TIMER_OFF}
+		if [[ -n ${DEBUG} ]] ; then
+			break
+		fi
+	done
+}
+
+if [[ "${1}" == "status" ]] ; then
+	cat ${STATUS_FILE}
+	exit 0
+fi
+
+main
